724224fcfee8b8d5e14997c807c4a00ef152b1b2
keerthiravilla <32122958+keerthiravilla@users.noreply.github.com>
2023-03-13 19:24:27 +0100

    Create ann models

---
+++ ANN_models/ann_models.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import EarlyStopping
+
+# Load the data and labels
+data = np.load('ftd_mri_data.npy')
+labels = np.load('ftd_mri_labels.npy')
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+
+# Scale the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Define the ANN model
+model = Sequential()
+model.add(Dense(units=64, activation='relu', input_dim=X_train.shape[1]))
+model.add(Dense(units=32, activation='relu'))
+model.add(Dense(units=1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
+model.fit(X_train, y_train, validation_split=0.2, epochs=100, batch_size=32, callbacks=[es])
+
+# Evaluate the model on the testing set
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f'Test loss: {loss}, Test accuracy: {accuracy}')
